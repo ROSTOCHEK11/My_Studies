c1f2e3bb3ba956a1843a222da226370980bb8b76
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-03-31 20:32:21 +0200

    Web Api mini project

---
+++ WebApi_MiniProject/ApiMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,34 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMiniProject.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AddressController : Controller
+	{
+		private readonly ILogger<AddressController> _logger;
+
+		public AddressController(ILogger<AddressController> logger)
+		{
+			_logger = logger;
+		}
+
+
+		[HttpPost]
+		public void Post([FromBody] AddressModel data)
+		{
+			_logger.LogInformation("The person was logged as {Address}", data);
+		}
+
+
+		// GET: PersonController
+		[HttpGet("Index")]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+	}
+
+}
---
+++ WebApi_MiniProject/ApiMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMiniProject.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : Controller
+	{
+		private readonly ILogger<PersonController> _logger;
+
+		public PersonController(ILogger<PersonController> logger)
+        {
+			_logger = logger;
+		}
+
+
+		[HttpPost]
+		public void Post([FromBody] PersonModel data)
+		{
+			_logger.LogInformation("The person was logged as {Person}", data);
+		}
+
+
+        // GET: PersonController
+        [HttpGet("Index")]
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+
+
+		
+	}
+}
