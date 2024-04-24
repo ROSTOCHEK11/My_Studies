4737a10be7b6c93135d3b454d1bd077d4cdf6df8
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-04-24 16:47:40 +0200

    Blazor Server Mini Project

---
+++ BlazorServer_MiniProject/BlazorServer_MiniProjectApp/BlazorServer_MiniProject/Models/AddressModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServer_MiniProject.Models;
+
+public class AddressModel
+{
+	[Required]
+	public string StreetAddress { get; set; }
+
+	[Required] 
+	public string City { get; set; }
+
+	[Required]
+	public string Region { get; set; }
+	
+	[Required]
+	public string PostalCode { get; set; }
+}
---
+++ BlazorServer_MiniProject/BlazorServer_MiniProjectApp/BlazorServer_MiniProject/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServer_MiniProject.Models;
+
+public class PersonModel
+{
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+	public string LastName { get; set; }
+
+
+}
