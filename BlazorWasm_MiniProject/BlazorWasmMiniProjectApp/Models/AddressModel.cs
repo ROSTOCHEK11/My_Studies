ef3496bdeddf0e7bfad332346c715d90beec2cc0
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-04-26 19:38:10 +0200

    Blazor Wasm mini project

---
+++ BlazorWasm_MiniProject/BlazorWasmMiniProjectApp/Models/AddressModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWasmMiniProject.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string StreetAddress { get; set; }
+
+    [Required]
+    public string City { get; set; }
+
+    [Required]
+    public string Region { get; set; }
+
+    [Required]
+    public string PostalCode { get; set; }
+}
