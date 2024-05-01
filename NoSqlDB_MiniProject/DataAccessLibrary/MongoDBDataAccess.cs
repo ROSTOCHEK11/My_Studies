be40b882623a9f5b947614350d5f7ffbffa83d30
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-05-01 22:30:57 +0200

    MongoDB and CosmosDB
    Mini project with using NoSql Db as MongoDB and CosmosDB

---
+++ NoSqlDB_MiniProject/DataAccessLibrary/MongoDBDataAccess.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+	public class MongoDBDataAccess
+	{
+
+		private IMongoDatabase db;
+
+        public MongoDBDataAccess(string dbName, string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            db = client.GetDatabase(dbName);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+			var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+		}
+
+        public void UpsertRecord<T>(string table, Guid id, T record) // find if the record exists | if YES - it will update it | if No - it will insert it
+        {
+			var collection = db.GetCollection<T>(table);
+
+            var result = collection.ReplaceOne(new BsonDocument("_id", id),
+											   record,
+											   new UpdateOptions { IsUpsert = true });
+		}
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+			var collection = db.GetCollection<T>(table);
+			var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+
+		}
+
+	}
+}
