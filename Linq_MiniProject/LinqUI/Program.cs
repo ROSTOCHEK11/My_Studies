9018e8651ca9e8cddbc0c90f3236370a757a2864
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-05-04 12:05:33 +0200

    Linq and Lamba Mini Project

---
+++ Linq_MiniProject/LinqUI/Program.cs
@@ -0,0 +1,113 @@
+﻿using LinqUI;
+using static System.Console;
+
+
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+
+		//LambdaTests();
+		LinqTests();
+
+
+
+		WriteLine("\nDone processing");
+		ReadLine();
+
+	}
+
+	private static void LinqTests()
+	{
+
+		var contacts = SampleData.GetContactData();
+		var addresses = SampleData.GetAddressData();
+
+
+		//var results = (from c in contacts
+		//			   where c.Addresses.Count > 1
+		//			   select c);
+
+
+		//var results = (from c in contacts
+		//			   join a in addresses
+		//			   on c.Id equals a.ContactId
+		//			   select new { c.FirstName, c.LastName, a.City, a.Region });
+
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName} from {item.City}, {item.Region}");
+		//}
+
+
+		//var results = (from c in contacts
+		//			   select new { c.FirstName, c.LastName, Addresses = addresses.Where(x => x.ContactId == c.Id)});
+
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName} - {item.Addresses.Count()}");
+		//}
+
+
+		var results = (from c in contacts
+					   select new { c.FirstName, c.LastName, Addresses = addresses.Where(a => c.Addresses.Contains(a.Id)) });
+
+		foreach (var item in results)
+		{
+			WriteLine($"{item.FirstName} {item.LastName} - {item.Addresses.Count()}");
+		}
+
+
+
+	}
+
+	private static void LambdaTests()
+	{
+
+		var data = SampleData.GetContactData();
+
+		//var results = data.Where(x => x.Addresses.Count > 1);
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName}");
+		//}
+
+
+		//var results = data.Select(x => x.FirstName);
+		//foreach (var item in results)
+		//{
+		//	WriteLine(item);
+		//}
+
+
+		//var results = data.Take(2);
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName}");
+		//}
+
+
+		//var results = data.Skip(2).Take(2);
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName}");
+		//}
+
+
+		//var results = data.OrderByDescending(x => x.LastName); // OrderBy()
+		//foreach (var item in results)
+		//{
+		//	WriteLine($"{item.FirstName} {item.LastName}");
+		//}
+
+
+
+	}
+
+
+
+
+
+}
